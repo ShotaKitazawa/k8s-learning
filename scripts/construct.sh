ce5200c9ba223aa0d5f36af06162b5cc2323cbba
ShotaKitazawa <skitazawa1121@gmail.com>
2019-06-28 17:34:21 +0900

    modify scripts

--- scripts/construct.sh
+++ scripts/construct.sh
@@ -23,29 +23,29 @@ fi
 if [ "$ARG" = "create" ]; then
   if [ "$NAMES" = "" ]; then echo "NAMES is undefined"; exit 1; fi
   if [ ! -e .state ]; then echo "$NAMES" > .state; else echo ".state exists"; exit 1; fi
-  gcloud compute networks create $NETWORK --subnet-mode custom
-  gcloud compute firewall-rules create $NETWORK --network=$NETWORK --rules tcp:22 --action allow
+  gcloud compute networks create $NETWORK --subnet-mode custom --project=$PROJECT
+  gcloud compute firewall-rules create $NETWORK --network=$NETWORK --rules tcp:22 --action allow --project=$PROJECT
   cnt=0
   for name in $NAMES; do
     cnt=$(($cnt+1))
     range=10.$cnt.0.0/24
-    gcloud compute networks subnets create $NETWORK-$name --network=$NETWORK --range=$range --region $REGION
-    gcloud compute firewall-rules create $NETWORK-$name --network=$NETWORK --source-ranges=$range --rules all --action allow
-    gcloud compute instances create $INSTANCE-$name-01 --image-family $IMAGE_FAMILY --image-project $IMAGE_PROJECT --subnet $NETWORK-$name --zone $ZONE
-    gcloud compute instances create $INSTANCE-$name-02 --image-family $IMAGE_FAMILY --image-project $IMAGE_PROJECT --subnet $NETWORK-$name --zone $ZONE
-    gcloud compute instances create $INSTANCE-$name-03 --image-family $IMAGE_FAMILY --image-project $IMAGE_PROJECT --subnet $NETWORK-$name --zone $ZONE
+    gcloud compute networks subnets create $NETWORK-$name --network=$NETWORK --range=$range --region $REGION --project=$PROJECT
+    gcloud compute firewall-rules create $NETWORK-$name --network=$NETWORK --source-ranges=$range --rules all --action allow --project=$PROJECT
+    gcloud compute instances create $INSTANCE-$name-01 --image-family $IMAGE_FAMILY --image-project $IMAGE_PROJECT --subnet $NETWORK-$name --zone $ZONE --project=$PROJECT
+    gcloud compute instances create $INSTANCE-$name-02 --image-family $IMAGE_FAMILY --image-project $IMAGE_PROJECT --subnet $NETWORK-$name --zone $ZONE --project=$PROJECT
+    gcloud compute instances create $INSTANCE-$name-03 --image-family $IMAGE_FAMILY --image-project $IMAGE_PROJECT --subnet $NETWORK-$name --zone $ZONE --project=$PROJECT
   done
 elif [ "$ARG" = "delete" ]; then
   if [ -e .state ]; then NAMES="$(cat .state)"; else echo ".state dont exist"; exit 1; fi
   for name in $NAMES; do
-    gcloud compute instances delete $INSTANCE-$name-01 -q
-    gcloud compute instances delete $INSTANCE-$name-02 -q
-    gcloud compute instances delete $INSTANCE-$name-03 -q
-    gcloud compute firewall-rules delete $NETWORK-$name -q
-    gcloud compute networks subnets delete $NETWORK-$name --region $REGION -q
+    gcloud compute instances delete $INSTANCE-$name-01 --zone $ZONE -q --project=$PROJECT
+    gcloud compute instances delete $INSTANCE-$name-02 --zone $ZONE -q --project=$PROJECT
+    gcloud compute instances delete $INSTANCE-$name-03 --zone $ZONE -q --project=$PROJECT
+    gcloud compute firewall-rules delete $NETWORK-$name -q --project=$PROJECT
+    gcloud compute networks subnets delete $NETWORK-$name --region $REGION -q --project=$PROJECT
   done
-  gcloud compute firewall-rules delete $NETWORK -q
-  gcloud compute networks delete $NETWORK -q
+  gcloud compute firewall-rules delete $NETWORK -q --project=$PROJECT
+  gcloud compute networks delete $NETWORK -q --project=$PROJECT
   rm -f .state
 else
   echo "invalid argument"
